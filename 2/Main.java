bb087567b925d80e67d63c8fef9a518f4c33df2c
Dmitry Legochev <114214910+gUmka07@users.noreply.github.com>
2023-10-31 19:34:25 +0700

    Create Main.java

---
+++ 2/Main.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+import ru.gb.server.ServerGUI;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new ServerGUI();
+    }
+}
